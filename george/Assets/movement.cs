b55984efd26609e129c0f94475c7b340a851e78b
CyrilJones9 <48595724+CyrilJones9@users.noreply.github.com>
2019-05-06 08:49:38 -0700

    a

---
+++ george/Assets/movement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour
+{
+    public float speed = 30;
+
+    void FixedUpdate()
+    {
+        float v = Input.GetAxisRaw("horizontal");
+        GetComponent<Rigidbody2D>().horizontal = new Vector2(0, v) * speed;
+    }
+}
+
+}
